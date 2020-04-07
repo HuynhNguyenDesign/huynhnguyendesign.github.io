955702f6533718a22725f6701a6e00ee7ed4b5b2
HuynhNguyen2207 <huynh.nguyen2207@gmail.com>
2020-04-07 16:04:10 +0700

    add animation

--- js/app.js
+++ js/app.js
@@ -1,43 +1,32 @@
-$(document).ready(function(){
+$("header.fas").click(function() {
+  $("header.fas").toggleClass("fa-bars fa-times");
+});
 
-  $("header.fas").click(function(){
-    $("header.fas").toggleClass("fa-bars fa-times");
-  });
+var currentYear = (new Date).getFullYear();
 
-  var currentYear = (new Date).getFullYear();
+$(".copyyear").text(currentYear);
 
-  $(".copyyear").text(currentYear);
+$(".slideshow").slick({
+  autoplay: true,
+  arrows: false,
+  dots: true
+});
 
-  $(".slideshow").slick({
-    autoplay: true,
-    arrows: false,
-    dots: true
-  });
-
-  // Start of Client Slide
-  $(".client-slide").slick({
-    autoplay: true,
-    arrows: false,
-    slidesToShow: 6,
-    responsive: [ {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
+// Start of Client Slide
+$(".client-slide").slick({
+  autoplay: true,
+  arrows: false,
+  slidesToShow: 6,
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
     }
-  ]
+  }]
 }); //End of Client Slide
 
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
-  }
-);
-
-}); // End of doc.ready
+$('.num').counterUp({
+  delay: 10,
+  time: 1500
+});
