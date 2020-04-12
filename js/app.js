88da82021ab037bdfa5ea75c4c1847a2b2346793
HuynhNguyen2207 <huynh.nguyen2207@gmail.com>
2020-04-13 01:44:13 +0700

    xs

--- js/app.js
+++ js/app.js
@@ -11,14 +11,31 @@ $(window).on("load", function(){
     $(".loader").addClass("loaded");
 
 
-    $(".ghost").ghosttyper({
-      messages: ["it recruitment at its finest", "simple process", "deep understanding", "accurate consulting"],
-      timeWrite: 20,
-      timeDelete: 10,
-      timePause: 2680
+    // $(".ghost").ghosttyper({
+    //   messages: ["it recruitment at its finest", "simple process", "deep understanding", "accurate consulting"],
+    //   timeWrite: 20,
+    //   timeDelete: 10,
+    //   timePause: 2680
+    // });
+
+    //   var typed = new Typed('#typed', {
+  	//     stringsElement: "header-strings",
+    //     stringsElement: null,
+    //     typeSpeed: 80,
+    //     backSpeed: 10,
+    //     backDelay: 700,
+    //     loop: true
+  	// });
+
+    var typed = new Typed('#typed', {
+      stringsElement: '#typed-strings',
+        typeSpeed: 80,
+        backSpeed: 10,
+        backDelay: 700,
+        loop: true
     });
 
-    $(".slideshow").delay("20").slick({
+    $(".slideshow").delay("100").slick({
       autoplay: true,
       arrows: false,
       autoplaySpeed: 2900,
